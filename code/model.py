a3323e969c77280cdfdffdd8c6ad83c877a86379
KaiTan2 <1016886174@qq.com>
2021-09-08 21:35:41 +0800

    Update model.py

--- code/model.py
+++ code/model.py
@@ -5,6 +5,62 @@ from torch.autograd import Variable
 
 from layers import GAT, InnerProductDecoder
 import numpy as np
+
+class VGAE(nn.Module):
+    def __init__(self, conv_para, deconv_para, fc1_num, fc2_num, latent_size, training):
+        super(VGAE, self).__init__()
+        # self.lstm = LSTMEncoder1(emb_size, hidden_size, layers_num, dropout)
+        self.training = training
+        #Encoder
+        self.conv_encoder = nn.ModuleList(nn.Conv2d(in_channels=para[0], out_channels=para[1], kernel_size=para[2], stride=para[3], padding=para[4])
+                                          for para in conv_para)
+        self.fc1 = nn.Linear(fc1_num[0], fc1_num[1])
+        # Latent space
+        self.fc21 = nn.Linear(fc2_num[0], fc2_num[1])
+        self.fc22 = nn.Linear(fc2_num[0], fc2_num[1])
+
+        # Decoder
+        self.latent_size = latent_size
+        self.fc3 = nn.Linear(fc2_num[1], fc2_num[0])
+        self.fc4 = nn.Linear(fc1_num[1], fc1_num[0])
+        self.deconv_decoder = nn.ModuleList(nn.ConvTranspose2d(in_channels=para[0], out_channels=para[1], kernel_size=para[2], stride=para[3], padding=para[4])
+                                          for para in deconv_para)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def encode(self, x):
+        conv_out = x
+        for conv_layer in self.conv_encoder:
+            conv_out = self.relu(conv_layer(conv_out))
+        conv_out = conv_out.view(conv_out.size(0), -1)
+        h1 = self.relu(self.fc1(conv_out))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z):
+        h3 = self.relu(self.fc3(z))
+        out = self.relu(self.fc4(h3))
+        out = out.view(out.size(0), self.latent_size[0], self.latent_size[1], self.latent_size[2])
+        for deconv_layer in self.deconv_decoder[:-1]:
+            out = self.relu(deconv_layer(out))
+
+        out = self.sigmoid(self.deconv_decoder[-1](out))
+        return out
+
+
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
 #自编码器
 class AutoEncoder(nn.Module):
     def __init__(self, input_dim, hidden_dim):
